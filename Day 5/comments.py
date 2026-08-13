d27dbe0254fcdaa18225ceceebf72ea91b0fb2c8
I-am-Som <somanathmaharanaofficial@gmail.com>
2026-08-13 14:42:27 +0530

    day 4 and 5

---
+++ Day 5/comments.py
@@ -0,0 +1,6 @@
+print("Hi how are you?? \n Are you doing fine??")
+# This is an example comments.
+
+
+''' This is an example multi-line comments. 
+It is the communcation between current coder/ developer and previous coder/developer!!!'''
---
+++ Day 5/escapeSequence.py
@@ -0,0 +1,8 @@
+print("Hello world how are you? \n Are you doing fine???")
+
+#example of escape sequence 
+print("Hi this is my new program and I told everyone that \"I practiced escape sequence\"")
+
+# In this above print command escape sequence is \"
+''' Escape sequences are the character/symbols that 
+ cannot be inserted into a string because of its preprogrammed/prefitted uses in programming.'''
